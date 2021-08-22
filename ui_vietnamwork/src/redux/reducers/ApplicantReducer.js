a5d87483a25c8646105ffeedd3b870a264a163a3
Du Duong <khaidu2002@gmail.com>
2021-08-22 09:28:58 -0500

    Completed NodeJS

--- ui_vietnamwork/src/redux/reducers/ApplicantReducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = {}
-
-export const ApplicantReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return { ...state }
-
-    }
-}
